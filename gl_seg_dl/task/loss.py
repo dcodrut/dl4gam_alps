ff02030cd36f1533b5e6c4cd11086e612ae23909
dcodrut <codrutandreidiaconu@gmail.com>
2024-09-02 01:03:04 +0200

    add focal loss

--- gl_seg_dl/task/loss.py
+++ gl_seg_dl/task/loss.py
@@ -2,6 +2,27 @@ import torch
 import torch.nn.functional as F
 
 
+def focal_loss(preds, targets, gamma=2, alpha=0.25, reduction='mean'):
+    # based on: https://pytorch.org/vision/main/_modules/torchvision/ops/focal_loss.html
+
+    # compute the binary cross-entropy loss
+    bce = F.binary_cross_entropy(preds, targets, reduction='none')
+
+    # compute the focal loss
+    preds_t = preds * targets + (1 - preds) * (1 - targets)
+    loss = bce * ((1 - preds_t) ** gamma)
+
+    if alpha >= 0:
+        alpha_t = alpha * targets + (1 - alpha) * (1 - targets)
+        loss = alpha_t * loss
+
+    if reduction == 'mean':
+        return loss.mean()
+    elif reduction == 'sum':
+        return loss.sum()
+    return loss
+
+
 def dice_loss(preds, targets, axes=None, eps=1e-6):
     intersection = (preds * targets).sum(axis=axes)
 
@@ -28,7 +49,7 @@ class MaskedLoss(torch.nn.Module):
         targets_masked = targets * mask
 
         # reduce the loss to a single dimension or one per batch element (for dice it is already reduced)
-        if self.metric in ('L2', 'L1', 'BCE'):
+        if self.metric in ('L2', 'L1', 'BCE', 'focal'):
             # compute the loss
             reduction = 'none' if samplewise else 'sum'
             if self.metric == 'L2':
@@ -37,6 +58,8 @@ class MaskedLoss(torch.nn.Module):
                 loss = F.l1_loss(preds_masked, targets_masked, reduction=reduction)
             elif self.metric == 'BCE':
                 loss = F.binary_cross_entropy(preds_masked, targets_masked, reduction=reduction)
+            elif self.metric == 'focal':
+                loss = focal_loss(preds_masked, targets_masked, reduction=reduction)
 
             loss = loss.sum(axes)
             den = mask.sum(axes)

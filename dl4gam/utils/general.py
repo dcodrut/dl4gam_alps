e9f7b1cc74f7cc2caf69e9962c8fa4c57044c7ba
dcodrut <codrutandreidiaconu@gmail.com>
2025-03-30 13:39:11 +0200

    switch to ProcessPoolExecutor (handles errors better); print the error before failing and the input

--- dl4gam/utils/general.py
+++ dl4gam/utils/general.py
@@ -1,7 +1,8 @@
-import multiprocessing
 import argparse
-from tqdm import tqdm
 import itertools
+from concurrent.futures import ProcessPoolExecutor, as_completed
+
+from tqdm import tqdm
 
 
 def str2bool(v):
@@ -19,6 +20,7 @@ def str2bool(v):
 def _fn_star(kwargs):
     fun = kwargs['fun']
     kwargs_others = {k: v for k, v in kwargs.items() if k != 'fun'}
+
     return fun(**kwargs_others)
 
 
@@ -38,15 +40,53 @@ def run_in_parallel(fun, num_procs=1, pbar=False, pbar_desc=None, **kwargs):
     if pbar_desc is None:
         fun_name = fun.func.__name__ if hasattr(fun, 'func') else fun.__name__
         pbar_desc = f'Running {fun_name} with {num_procs} process(es)'
-    with tqdm(total=arg_lens[0], desc=pbar_desc, disable=not pbar) as pbar:
-        if num_procs > 1:
-            with multiprocessing.Pool(num_procs) as pool:
-                for res in pool.imap_unordered(_fn_star, kwargs_flatten):
-                    all_res.append(res)
-                    pbar.update()
-        else:
+
+    if num_procs > 1:
+        with ProcessPoolExecutor(max_workers=num_procs) as executor:
+            futures = []
+            future_to_input = {}  # track submitted inputs
+            iterator = iter(kwargs_flatten)
+
+            # Submit initial batch
+            for _ in range(num_procs):
+                try:
+                    kw = next(iterator)
+                    future = executor.submit(_fn_star, kw)
+                    futures.append(future)
+                    future_to_input[future] = kw
+                except StopIteration:
+                    break
+
+            with tqdm(total=len(kwargs_flatten), desc=pbar_desc, disable=not pbar) as pbar:
+                while futures:
+                    for future in as_completed(futures):
+                        futures.remove(future)
+                        kw = future_to_input.pop(future)
+
+                        try:
+                            result = future.result()
+                            all_res.append(result)
+                        except Exception as e:
+                            print(f"[ERROR]: {e}\nInput:\n {kw} \n")
+                            raise e
+
+                        pbar.update()
+
+                        # Submit next task
+                        try:
+                            kw = next(iterator)
+                            future = executor.submit(_fn_star, kw)
+                            futures.append(future)
+                            future_to_input[future] = kw
+                        except StopIteration:
+                            pass
+
+                        break  # back to waiting with updated futures
+    else:
+        with tqdm(total=arg_lens[0], desc=pbar_desc, disable=not pbar) as pbar:
             for crt_args in kwargs_flatten:
                 res = _fn_star(crt_args)
                 all_res.append(res)
                 pbar.update()
+
     return all_res
